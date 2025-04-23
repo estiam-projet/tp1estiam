7d3bc4b19036aa73708a8b4fe958adf424f573a1
Nicolas Koumakoye <patritkoumakoyeloicnicolas@gmail.com>
2025-04-23 16:11:55 +0200

    Ajout de la fonctionnalité CLI pour la calculatrice (branche feature/cli-interface)

---
+++ calculator.py
@@ -0,0 +1,16 @@
+ # calculator.py
+
+def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("Division par zéro interdite")
+    return a / b
+
